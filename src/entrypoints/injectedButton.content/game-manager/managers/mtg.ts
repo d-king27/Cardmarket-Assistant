037e3374f6cfe92231dee3a85ccff5d14ea97eb8
d-king27 <kingd8883@gmail.com>
2026-07-13 21:59:50 +0100

    fix: parse cardmarket page context safely

--- src/entrypoints/injectedButton.content/game-manager/managers/mtg.ts
+++ src/entrypoints/injectedButton.content/game-manager/managers/mtg.ts
@@ -6,6 +6,7 @@ import GenericGameManager from './generic';
 import type { BaseColumnMapping, CommonParsedRowFields } from './generic';
 import { compareNormalized } from '../../../../utils';
 import type { TranslationKey } from '../../../../utils';
+import { parseCardmarketPageContext } from '../../cardmarket/pageContext';
 import { cardmarketSelectors } from '../../cardmarket/selectors';
 import { parseBoolean } from '../utils';
 
@@ -47,11 +48,14 @@ class MtgGameManager extends GenericGameManager<'set' | 'isFoil', { set: string,
     let set = columnMapping['set'] ? String(rawRowData[columnMapping['set']]) : '';
     let enabled = parsedData.enabled;
     if (set) {
-      const paramsCode = Number(new URLSearchParams(window.location.search).get('idExpansion'));
+      const pageContext = parseCardmarketPageContext();
       const data = await matchSetToCardmarketId(set);
       if (data) {
         set = data.code;
-        if (data.cardmarketId !== paramsCode) enabled = false;
+        if (
+          pageContext.expansionId.kind === 'valid'
+          && data.cardmarketId !== pageContext.expansionId.value
+        ) enabled = false;
       }
       else {
         set = '';
